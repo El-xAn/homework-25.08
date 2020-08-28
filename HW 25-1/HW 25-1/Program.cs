f40edc44aff97b82edf30094e204d51c9e467eb1
El-xAn <hidefinatio123@gmail.com>
2020-08-28 10:02:55 +0400

    new branch

---
+++ HW 25-1/HW 25-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HW_25_1
+{
+    class Program
+    {
+        static int step(int n)
+        {
+            int a = 2;
+            
+            for (int i = 2; i <= n; i++)
+            {
+                a = a * 2;                              
+            }
+            return a;
+        }
+
+        static int sum(int n)
+        {
+            int summa = 0;
+            if (n>=2)
+            {
+                summa = step(n) + sum(n - 1);                               
+            } 
+            return summa;
+        }   
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(10));
+        }
+    }
+}
---
+++ HW 25-4/HW 25-4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HW_25_4
+{
+    class Program
+    {
+        static int interval(int a, int b)
+        {
+            if (a > b)
+            {
+                int c;
+                c = a;
+                a = b;
+                b = a;
+            }
+            if (a == b)
+            {
+                return b;
+            }
+            else 
+            {
+                return a + interval(a + 1, b);
+            }  
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvodite 1oe cislo:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vvodite 2oe cislo:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Summa cisel diapazona mejdu nimi: {interval(a + 1, b - 1)}");
+        }  
+            
+    }
+}
